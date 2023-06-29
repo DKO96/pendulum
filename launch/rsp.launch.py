014a5dcbcf185cb5e7f91da0272af8796f8f5316
DKO96 <danielwjkhoo@gmail.com>
2023-06-29 00:29:01 -0400

    pendulum in rviz

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -12,30 +12,45 @@ import xacro
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
+    # urdf file
+    pkg_path = os.path.join(get_package_share_directory('pendulum'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
-    # Create a robot_state_publisher node
+    # robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
-    node_robot_state_publisher = Node(
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[params]
     )
 
+    # rviz node
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen'
+    )
 
-    # Launch!
+    # joint state publisher node
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    )
+
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        robot_state_publisher_node,
+        rviz_node,
+        joint_state_publisher_node,
     ])
